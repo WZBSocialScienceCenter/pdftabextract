5efe11807d40b54745c6fb08b6c2733b2269c82a
Markus Konrad <markus.konrad@wzb.eu>
2016-06-01 10:54:12 +0200

    + functions for handling text elements

--- ipynotes.py
+++ ipynotes.py
@@ -11,12 +11,12 @@ tree = ET.parse('testxmls/1992_93.pdf.xml')
 root = tree.getroot()
 
 #%%
+def sorted_by_attr(texts, attr):
+    return sorted(texts, key=lambda x: x[attr])
+
 def parse_pages(root):
     pages = {}
-    
-    def sorted_by_attr(texts, attr):
-        return sorted(texts, key=lambda x: x[attr])
-    
+        
     for p in root.findall('page'):
         p_num = int(p.attrib['number'])
         page = {
@@ -40,12 +40,52 @@ def parse_pages(root):
                 'right': t_left + t_width,
                 'width': t_width,
                 'height': t_height,
+                'center_x': t_left + t_width / 2,
+                'center_y': t_top + t_height / 2,
+                'value': t.text,  # only for easy identification during debugging. TODO: delete
                 'xmlnode': t
             }
             
             page['texts'].append(text)
             
-        page['texts_leftright'] = sorted_by_attr(page['texts'], 'left')
-        page['texts_topdown'] = sorted_by_attr(page['texts'], 'top')
+        # page['texts_leftright'] = sorted_by_attr(page['texts'], 'center_x')
+        # page['texts_topdown'] = sorted_by_attr(page['texts'], 'center_y')
         pages[p_num] = page
+
+    return pages
+
+
+def get_bodytexts(page):
+    miny = page['height'] * HEADER_RATIO
+    maxy = page['height'] * (1 - FOOTER_RATIO)
     
+    return list(filter(lambda t: t['top'] >= miny and t['bottom'] <= maxy, page['texts']))    
+
+
+def divide_texts_horizontally(page, texts=None):
+    if texts is None:
+        texts = page['texts']
+    
+    divide_x = page['width'] * DIVIDE_RATIO
+    lefttexts = list(filter(lambda t: t['right'] <= divide_x, texts))
+    righttexts = list(filter(lambda t: t['right'] > divide_x, texts))
+    
+    assert len(lefttexts) + len(righttexts) == len(texts)
+    
+    return lefttexts, righttexts
+    
+    
+#%%
+pages = parse_pages(root)
+
+HEADER_RATIO = 0.1
+FOOTER_RATIO = 0.1
+DIVIDE_RATIO = 0.5
+
+page = pages[35]
+
+bodytexts = get_bodytexts(page)
+div_texts = divide_texts_horizontally(page, bodytexts)
+
+text = div_texts[0]
+
