d982c570d1a8447b2c4ae897bb699316de9fd420
Markus Konrad <markus.konrad@wzb.eu>
2017-02-09 16:44:01 +0100

    full doc. of functions in common.py

--- pdftabextract/common.py
+++ pdftabextract/common.py
@@ -8,8 +8,6 @@ Created on Tue Jun  7 10:49:35 2016
 """
 
 
-#%%
-
 import xml.etree.ElementTree as ET
 from collections import OrderedDict
 import json
@@ -31,12 +29,20 @@ DIRECTION_VERTICAL = 'v'
 #%% I/O
 
 def read_xml(fname):
+    """
+    Read an XML file <fname> which can be later parsed with parse_pages. Uses Python's xml.etree.ElementTree.
+    Return a tuple with (XML tree object, tree root element)
+    """
     tree = ET.parse(fname)
     root = tree.getroot()
     
     return tree, root
 
+
 class JSONEncoderPlus(json.JSONEncoder):
+    """
+    Extended JSONEncoder class to be used in save_page_grids.
+    """
     def default(self, o):
        try:
            iterable = iter(o)
@@ -47,7 +53,13 @@ class JSONEncoderPlus(json.JSONEncoder):
        # Let the base class default method raise the TypeError
        return super().default(self, o)
 
+
 def save_page_grids(page_grids, output_file):
+    """
+    Save a dict <page_grids> with page number -> grid structure as JSON to <output_file>.
+    The grid can be generated with extract.make_grid_from_positions.
+    This file can be displayed with pdf2xml-viewer.
+    """
     with open(output_file, 'w') as f:
         json.dump(page_grids, f, cls=JSONEncoderPlus)
 
@@ -61,15 +73,18 @@ def parse_pages(root, load_page_nums=None, require_image=False, only_load_toplef
     will be loaded).
     <only_load_topleft_image> if there's more than one background image per page, use the one with top="0" and left="0"
     position.
+    
+    Return an OrderedDict with page number -> page dict mapping.
     """
     pages = OrderedDict()
     
-    for p in root.findall('page'):
+    for p in root.findall('page'):  # parse all pages
         p_num = int(p.attrib['number'])
         
         if load_page_nums is not None and p_num not in load_page_nums:
             continue
         
+        # find all images of the page
         p_images = p.findall('image')
         if p_images:            
             if len(p_images) == 1:
@@ -89,6 +104,7 @@ def parse_pages(root, load_page_nums=None, require_image=False, only_load_toplef
             else:
                 imgfile = None
         
+        # create the page dict structure
         page = {
             'number': p_num,
             'image': imgfile,
@@ -98,6 +114,7 @@ def parse_pages(root, load_page_nums=None, require_image=False, only_load_toplef
             'xmlnode': p,
         }
         
+        # add the text boxes to the page
         for t in p.findall('text'):
             tdict = create_text_dict(t)
             trect = rect(tdict['topleft'], tdict['bottomright'])
@@ -116,6 +133,9 @@ def parse_pages(root, load_page_nums=None, require_image=False, only_load_toplef
 
 
 def create_text_dict(t, value=None):
+    """
+    From an XML element <t>, create a text box dict structure and return it.
+    """
     t_width = int(float(t.attrib['width']))
     t_height = int(float(t.attrib['height']))
 
@@ -132,12 +152,20 @@ def create_text_dict(t, value=None):
 
 
 def update_text_xmlnode(t, attr, val, round_float=True):
+    """
+    Set the attribute <attr> to <val> of the XML node connected with text box dict <t>.
+    """
     if round_float:
         val = int(round(val))
     t['xmlnode'].attrib[attr] = str(val)
 
     
 def update_text_dict_pos(t, pos, update_node=False):
+    """
+    Update text box <t>'s position and set it to <pos>, where the first element of <pos> is the x and the second is the
+    y coordinate.
+    If <update_node> is True, also set the respective attributes in the respective XML node of the text box.
+    """
     t_top = pos[1]
     t_left = pos[0]
     t_bottom = t_top + t['height']
@@ -217,11 +245,12 @@ def levenshtein(source, target):
 
 def fill_array_a_with_values_from_b(a, b, fill_indices):
     """
-    Fill array <a> with values specified by <fill_indices> from <b>.
+    Fill array <a> with values from <b> taking values from indicies specified by <fill_indices>.
     
     Example:
     fill_array_a_with_values_from_b(np.array(list('136')), np.array(list('abcdef')), [1, 3, 4])
     
+    indices:      1       3   4       <- in "b"
     result: ['1' 'b' '3' 'd' 'e' '6']
     """
     if type(a) is not np.ndarray:
@@ -271,14 +300,20 @@ def flatten_list(l):
 
 
 def any_a_in_b(a, b):
+    """Return true if any element *s* of <a> also exists in <b>."""
     return any(s in b for s in a)
 
     
 def all_a_in_b(a, b):
+    """Return true if all elements *s* of <a> also exist in <b>."""
     return all(s in b for s in a)
 
 
 def updated_dict_copy(d_orig, d_upd):
+    """
+    Create a copy of <d_orig>, update it with <d_upd> and return that updated copy.
+    """
     d = d_orig.copy()
     d.update(d_upd)
+    
     return d
