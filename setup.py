74f5998dc332f9a5d3b059dcc1555f9ce1639713
Markus Konrad <markus.konrad@wzb.eu>
2017-02-16 11:14:52 +0100

    requirements and setup updated

--- setup.py
+++ setup.py
@@ -11,8 +11,9 @@ setup(
 
     description='A set of tools for data mining (OCR-processed) PDFs',
     long_description="""This repository contains a set of tools written in Python 3 with the aim to extract tabular
-data from (OCR-processed) PDF files. Before these files can be processed they need to be converted to XML files in
-pdf2xml format using poppler utils.""",
+data from scanned and OCR-processed documents available as PDF files. Before these files can be processed they need
+to be converted to XML files in pdf2xml format using poppler utils. Further information and examples can be found
+in the github repository.""",
 
     url='https://github.com/WZBSocialScienceCenter/pdftabextract',
 
